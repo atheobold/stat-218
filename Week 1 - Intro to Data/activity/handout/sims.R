576db3d4c868d1335a9d7a3723e2995295477e69
Allison Theobold <atheobol@calpoly.edu>
2022-03-29 15:36:28 -0700

    materials for running simulations for class

--- Week 1 - Intro to Data/activity/handout/sims.R
+++ Week 1 - Intro to Data/activity/handout/sims.R
@@ -13,12 +13,19 @@ data_09 <- read_xlsx(here::here("Week 1 - Intro to Data",
                                 "activity", 
                                 "handout", 
                                 "correct_guesses.xlsx"), 
-                     sheet = "09")
+                     sheet = "09") %>% 
+  mutate(Correct_Guess = as.factor(Correct_Guess)
+         )
 
+## infer procedure doesn't work if you have 100% responses of one category!
 
-data_07 %>% 
+p_hat <- data_09 %>% 
+  specify(response = Correct_Guess, success = "Y") %>% 
+  calculate(stat = "prop")
+
+data_09 %>% 
   specify(response = Correct_Guess, success = "Y") %>% 
   hypothesize(null = "point", p = 0.5) %>% 
-  generate(reps = 1000, type = "bootstrap") %>% 
+  generate(reps = 1000) %>% 
   calculate(stat = "prop")
 
